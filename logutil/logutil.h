a2056d6b9726150338c5b3daa278173c3506a1b4
Elias Benali <ebenali@tradeterminal.io>
2022-11-12 11:58:56 -0800

    refactor(logger): lints; noexcept singleton getter

--- logutil/logutil.h
+++ logutil/logutil.h
@@ -18,113 +18,121 @@
 #include <string>
 
 template <typename Clock = std::chrono::system_clock>
-static inline std::string isoDate(
-    std::chrono::time_point<Clock> tp = Clock::now()) {
+static inline auto isoDate(std::chrono::time_point<Clock> tp = Clock::now())
+    -> std::string {
   return date::format("%FT%TZ",
                       std::chrono::floor<std::chrono::milliseconds>(tp));
 }
 
 struct Logger {
-  Logger(std::ostream &ost = std::cout, std::ostream &est = std::cerr)
-      : outStream{&ost}, errStream{&est} {}
+  explicit Logger(std::ostream &ost = std::cout, std::ostream &est = std::cerr)
+      : outStream_{&ost}, errStream_{&est} {}
   template <typename... _Args>
-  std::ostream &Info(const _Args &...args) {
-    return (verbosity_ >= Verbosity::INFO)
-               ? output(std::chrono::system_clock::now(), outStream, infoChalk,
+  auto Info(const _Args &...args) -> std::ostream & {
+    return (verbosity_ >= Verbosity::kInfo)
+               ? output(std::chrono::system_clock::now(), outStream_, kInfoChalk,
                         "[II]", args...)
-               : *outStream;
+               : *outStream_;
   }
 
   template <typename... _Args>
-  std::ostream &Warn(const _Args &...args) {
-    return (verbosity_ >= Verbosity::WARN)
-               ? output(std::chrono::system_clock::now(), outStream, warnChalk,
+  auto Warn(const _Args &...args) -> std::ostream & {
+    return (verbosity_ >= Verbosity::kWarn)
+               ? output(std::chrono::system_clock::now(), outStream_, kWarnChalk,
                         "[WW]", args...)
-               : *outStream;
+               : *outStream_;
   }
 
   template <typename... _Args>
-  std::ostream &Error(const _Args &...args) {
-    return (verbosity_ >= Verbosity::ERR)
-               ? output(std::chrono::system_clock::now(), errStream, errorChalk,
+  auto Error(const _Args &...args) -> std::ostream & {
+    return (verbosity_ >= Verbosity::kErr)
+               ? output(std::chrono::system_clock::now(), errStream_, kErrorChalk,
                         "[EE]", args...)
-               : *errStream;
+               : *errStream_;
   }
   template <typename... _Args>
-  std::ostream &Debug(const _Args &...args) {
-    return (verbosity_ >= Verbosity::DEBUG)
-               ? output(std::chrono::system_clock::now(), errStream, debugChalk,
+  auto Debug(const _Args &...args) -> std::ostream & {
+    return (verbosity_ >= Verbosity::kDebug)
+               ? output(std::chrono::system_clock::now(), errStream_, kDebugChalk,
                         "[DD]", args...)
-               : *errStream;
+               : *errStream_;
   }
   template <typename... _Args>
-  std::ostream &Trace(const _Args &...args) {
-    return (verbosity_ >= Verbosity::TRACE)
-               ? output(std::chrono::system_clock::now(), errStream, traceChalk,
+  auto Trace(const _Args &...args) -> std::ostream & {
+    return (verbosity_ >= Verbosity::kTrace)
+               ? output(std::chrono::system_clock::now(), errStream_, kTraceChalk,
                         "[TT]", args...)
-               : *errStream;
+               : *errStream_;
   }
 
   void setUnifiedOutput(bool flag) { this->unified_output_ = flag; }
 
   enum class Verbosity : unsigned {
-    QUIET = 0,
-    ERR,
-    WARN,
-    INFO,
-    DEBUG,
-    TRACE,
+    kQuiet = 0,
+    kErr,
+    kWarn,
+    kInfo,
+    kDebug,
+    kTrace,
   };
 
   void setVerbosity(Verbosity verbosity) { verbosity_ = verbosity; }
 
  protected:
-  template <typename Clock, typename _fmtType, typename... _Args>
-  std::ostream &output(std::chrono::time_point<Clock> &&tm, std::ostream *ost,
-                       const _fmtType &fmt = chalk::fg::White,
-                       const _Args &...args) {
-    const std::chrono::duration<float> deltaT =
-        tm - ((ost == outStream && !this->unified_output_) ? lastTimes[0]
-                                                           : lastTimes[1]);
-    lastTimes[(ost == outStream && !this->unified_output_) ? 0 : 1] = tm;
-    *ost << (tsChalk(isoDate(tm)) + "  " + fmt(logN(args...)) + " +" +
-             dtChalk(std::to_string(deltaT.count()) + "s") + "\n");
+  template <typename Clock, typename FmtType, typename... _Args>
+  auto output(std::chrono::time_point<Clock> &&tm, std::ostream *ost,
+              const FmtType &fmt = chalk::fg::White, const _Args &...args)
+      -> std::ostream & {
+    const std::chrono::duration<float> delta_t =
+        tm - ((ost == outStream_ && !this->unified_output_) ? lastTimes_[0]
+                                                           : lastTimes_[1]);
+    lastTimes_[(ost == outStream_ && !this->unified_output_) ? 0 : 1] = tm;
+    *ost << (kTsChalk(isoDate(tm)) + "  " + fmt(logN(args...)) + " +" +
+             kDtChalk(std::to_string(delta_t.count()) + "s") + "\n");
     return *ost;
   }
   template <typename _Arg0, typename... Args>
-  std::string logN(_Arg0 const &arg0, const Args &...args) {
+  auto logN(_Arg0 const &arg0, const Args &...args) -> std::string {
     std::ostringstream oss{};
     oss << " " << arg0;
     return oss.str() + logN(args...);
   }
-  std::string logN() { return ""; }
+  static auto logN() -> std::string { return ""; }
 
  private:
-  std::ostream *outStream, *errStream;
-  std::chrono::time_point<std::chrono::system_clock> lastTimes[2]{
+  std::ostream *outStream_, *errStream_;
+  std::chrono::time_point<std::chrono::system_clock> lastTimes_[2]{
       std::chrono::system_clock::now(), std::chrono::system_clock::now()};
 
   bool unified_output_ = false;
-  Verbosity verbosity_ = Verbosity::DEBUG;
+  Verbosity verbosity_ = Verbosity::kDebug;
 
-  static constexpr auto tsChalk =
+  static constexpr auto kTsChalk =
       chalk::compose(chalk::fg::Green, chalk::fmt::Bold);
-  static constexpr auto dtChalk =
+  static constexpr auto kDtChalk =
       chalk::compose(chalk::fg::BrightCyan, chalk::fmt::Bold);
-  static constexpr auto infoChalk = chalk::fg::White;
-  static constexpr auto errorChalk = chalk::fg::Red;
-  static constexpr auto warnChalk = chalk::fg::Yellow;
-  static constexpr auto debugChalk = chalk::fg::Magenta;
-  static constexpr auto traceChalk =
+  static constexpr auto kInfoChalk = chalk::fg::White;
+  static constexpr auto kErrorChalk = chalk::fg::Red;
+  static constexpr auto kWarnChalk = chalk::fg::Yellow;
+  static constexpr auto kDebugChalk = chalk::fg::Magenta;
+  static constexpr auto kTraceChalk =
       chalk::compose(chalk::fmt::Italic, chalk::fg::Blue);
 
  public:
   /**
    * singleton style
    */
-  static Logger &get() {
-    static std::unique_ptr<Logger> singleton = std::make_unique<Logger>();
-    return *singleton;
+  static auto get() noexcept -> Logger & {
+    try {
+      static std::unique_ptr<Logger> singleton = std::make_unique<Logger>();
+      return *singleton;
+    } catch (std::exception const &err) {
+      std::cerr << fmt::format(
+          "{}:{}:{}: caught exception while constructing Logger singleton: "
+          "{}\n",
+          __FILE__, __LINE__, __func__, err.what());
+      std::terminate();
+    }
   }
 };
 
@@ -132,7 +140,7 @@ struct Logger {
 
 #if (__cplusplus >= 202002L)
 template <ssize_t N>
-consteval const char *_basename(const char (&path)[N]) {
+consteval auto _basename(const char (&path)[N]) -> const char * {
   ssize_t i;
   for (i = N - 1; i >= 0; --i)
     if (path[i] == '/') break;
